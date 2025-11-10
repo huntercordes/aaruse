4e305afe356109c97d92c8ff3fd146944cf8f046
Evelina Sneidere <eaa24evsn@students.eaaa.dk>
2025-11-10 15:20:43 +0100

    set up App.jsx structure and imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,77 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import { useAuth } from "./context/AuthContext";
+import PostPage from "./pages/PostPage";
+import PostDetailsPage from "./pages/PostDetailsPage";
+import FavouritesPage from "./pages/FavouritesPage";
+import { HomeProvider } from "./context/HomeContext";
+
+import SignupPage from "./pages/SignupPage";
+import ProfilePage from "./pages/ProfilePage";
+
+function ProtectedRoute({ children }) {
+  const { user, loading } = useAuth();
+
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <Navigate to="/" />;
+  return children;
+}
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <HomeProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/post"
+            element={
+              <ProtectedRoute>
+                <PostPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/favourites"
+            element={
+              <ProtectedRoute>
+                <FavouritesPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/post/:id"
+            element={
+              <ProtectedRoute>
+                <PostDetailsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </HomeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
